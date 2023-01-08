d11725ce6803dc4b7b7d13f400b863b934ecabb0
Emmanuel Gyabaah <gyabemman@gmail.com>
2023-01-08 17:44:22 +0000

    Bare spring boot proect from initializer

---
+++ src/main/java/com/egyabaah/FinanceKing/FinanceKingApplication.java
@@ -0,0 +1,13 @@
+package com.egyabaah.FinanceKing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceKingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egyabaah/FinanceKing/FinanceKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egyabaah.FinanceKing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceKingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
