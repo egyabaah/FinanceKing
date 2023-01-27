06a791488e106e194d99a97b42f108fb4c59b26f
Emmanuel Gyabaah <gyabemman@gmail.com>
2023-01-27 13:08:02 +0000

    Addition of a const to check whether user is logged in or not

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home  from './Home';
 import { Books } from './pages/books';
 import Register from './pages/Register';
+import Navbar from './components/Navbar';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
+  <>
+  <Navbar isLoggedIn= {isLoggedIn} />
   <Routes>
-    <Route path="/" element={<Home />} />
+    <Route path="/" element={<Home />}  />
     <Route path="/books" element={<Books />} />
     <Route path="/register" element={<Register />} />
   </Routes>
+    </>
   )
 }
 
