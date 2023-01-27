aca80ffe4d85cfe9a64ca3158296812e29e25ec7
Emmanuel Gyabaah <gyabemman@gmail.com>
2023-01-27 13:07:09 +0000

    Testing of useNavigate with state

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import AccountService from "./services/accountService";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Home() {
+
  const [jsonob1, setJsonob1] = useState(); 
+ const navigate = useNavigate();
+ const location = useLocation();
  async function getAccounts(){
   let response = await AccountService.getAll();
   let jsonob = await response.data;
-  setJsonob1(jsonob)
-  console.log(jsonob)
+  setJsonob1(jsonob);
+  console.log(jsonob);
+  alert(location.state.message);
  }
  useEffect(()=>{
   getAccounts();
  }, [])
+ function mmn(){
+  navigate("/register")
+ }
  return (
   <>
   <h1>Hello, Home</h1>
@@ -19,7 +27,8 @@ function Home() {
   {jsonob1 ? jsonob1.map((account)=>{
    if(account != undefined){
 
-    return <h1 key={account.id}>{account.firstName}</h1>
+    return <button type="button" key={account.id} onClick={mmn}>{account.firstName}</button>
+    // <h1 key={account.id}>{account.firstName}</h1>
    }
   }): null}
   </>
